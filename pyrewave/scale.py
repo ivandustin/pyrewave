e8e61fc53fdbc9fbd86bfbbc651f7cf59109c01d
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-12-08 21:58:41 +0800

    Update scale

--- pyrewave/scale.py
+++ pyrewave/scale.py
@@ -2,9 +2,8 @@ from jax.numpy import iinfo
 from .core.scale import scale as scale_function
 
 
-def scale(temporary_type, target_type, input):
-    x = input.astype(temporary_type)
-    n = x.max()
+def scale(target_type, x):
     m = iinfo(target_type).max
+    n = x.max()
     y = scale_function(m, n, x)
     return y.astype(target_type)
