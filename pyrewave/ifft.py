afc570ea53ea98834852cbd40e3412e5492f3621
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-12-08 21:32:59 +0800

    Fix

--- pyrewave/ifft.py
+++ pyrewave/ifft.py
@@ -2,13 +2,13 @@ from functools import partial
 from jax.numpy.fft import fftfreq
 from jax.numpy import arange
 from jax import jit, vmap
-from pyrewave.ifft import ifft
+from pyrewave.core.ifft import ifft as ifft_function
 
 
 def ifft(batch_size, new_sample_rate, sample_rate, fourier):
     frequencies = fftfreq(fourier.size, 1 / sample_rate)
     time = arange(0, fourier.size / sample_rate, 1 / new_sample_rate)
-    function = jit(vmap(partial(ifft, frequencies, fourier)))
+    function = jit(vmap(partial(ifft_function, frequencies, fourier)))
     for i in range(0, time.size, batch_size):
         value = function(time[i : i + batch_size])
         yield value
